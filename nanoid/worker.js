b9b5379124b1e759b3bc68e42037f18040359c60
matthew vogt <matthew@matthewcv.com>
2020-04-03 16:46:30 -0500

    measuring nanoid perf

--- nanoid/worker.js
+++ nanoid/worker.js
@@ -1,10 +1,72 @@
-import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 
+// borrowed from https://github.com/ai/nanoid
+function nanoid_string() {
+    let id = '',
+        size = 21,
+        bytes = crypto.getRandomValues(new Uint8Array(size))
+
+    // A compact alternative for `for (var i = 0; i < step; i++)`.
+    while (size--) {
+        // It is incorrect to use bytes exceeding the alphabet size.
+        // The following mask reduces the random byte in the 0-255 value
+        // range to the 0-63 value range. Therefore, adding hacks, such
+        // as empty string fallback or magic numbers, is unneccessary because
+        // the bitmask trims bytes down to the alphabet size.
+        let byte = bytes[size] & 63
+        if (byte < 36) {
+        // `0-9a-z`
+        id += byte.toString(36)
+        } else if (byte < 62) {
+        // `A-Z`
+        id += (byte - 26).toString(36).toUpperCase()
+        } else if (byte < 63) {
+        id += '_'
+        } else {
+        id += '-'
+        }
+    }
+    return id
+}
+
+function nanoid_array() {
+    let id = [],
+        size = 21,
+        bytes = crypto.getRandomValues(new Uint8Array(size))
+
+    // A compact alternative for `for (var i = 0; i < step; i++)`.
+    while (size--) {
+        // It is incorrect to use bytes exceeding the alphabet size.
+        // The following mask reduces the random byte in the 0-255 value
+        // range to the 0-63 value range. Therefore, adding hacks, such
+        // as empty string fallback or magic numbers, is unneccessary because
+        // the bitmask trims bytes down to the alphabet size.
+        let byte = bytes[size] & 63
+        if (byte < 36) {
+        // `0-9a-z`
+        id.push( byte.toString(36) )
+        } else if (byte < 62) {
+        // `A-Z`
+        id.push((byte - 26).toString(36).toUpperCase())
+        } else if (byte < 63) {
+        id.push( '_')
+        } else {
+        id.push('-')
+        }
+    }
+    return id.join('')
+}
+// above 2 methods don't seem to make much difference in memory usage or time. 
+// probalby prefer string concat version since it is fewer bytes of JS. 
+var nanoid = nanoid_string;
+
+console.log(self)
 
 addEventListener('message', evt => {
     console.log('worker recieved message', evt)
     if(evt.data.numIds && evt.data.numIds > 0) {
+        //setTimeout(() => {
         generateSomeIds(evt.data.numIds)
+        //},0)
     }
 })
 
@@ -18,7 +80,8 @@ var set = new Set();
 
 function generateSomeIds(num ) {
 
-    console.log("start making ids")
+    var start = performance.now()
+    console.log("start making ids", start)
 
     for(let count = 1; count <= num; count++) {
 
@@ -50,8 +113,8 @@ function generateSomeIds(num ) {
 
 
     }
-    //console.log(idChars)
-    console.log("done making ids")
+    var done = performance.now();
+    console.log("done making ids", start,done, done-start)
     postMessage({idCharDistributions: idChars, numGenerated: num, totalGenerated: set.size})
     console.log("posted message")
 
